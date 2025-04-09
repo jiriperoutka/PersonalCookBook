64e351d320675720e1b408e0bd0b0d1444608bad
Jiří Peroutka <peroutkag@gmail.com>
2025-04-09 11:56:32 +0200

    "BE preparation"

---
+++ .venv/backend/config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+load_dotenv()
+
+uri = f"mongodb+srv://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASS')}@{os.getenv('MONGO_CLUSTER')}/?retryWrites=true&w=majority"
+
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+try:
+    client.admin.command('ping')
+    print("Ping OK – MongoDB is connected.")
+except Exception as e:
+    print("Connection error:", e)
